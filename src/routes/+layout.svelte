462ace72fb3e139e73a364d259d7cdab6d3de337
stud <stud@it233-13.almetpt.local>
2025-03-20 18:29:48 +0300

    Чурка

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import Heder from '$lib/components/Heder.svelte';
+import {Container,Stules} from '@svelte'
+const {children} = $props();
+</script>
+<Stules>
+    <Container>
+        </Heder>
+            {@render children()}
+       
+    </Container>
+
+
